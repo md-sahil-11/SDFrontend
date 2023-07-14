b0579cb403260a3a141778d1ce42d4b752a7d747
Md Sahil Alam <mdsahilalam11@gmail.com>
2023-07-14 20:47:56 +0530

    minor

--- src/services/base.js
+++ src/services/base.js
@@ -1,18 +1,12 @@
 import axios from "axios";
 import { getItemFromLocalStorage, removeAllItemFromLocalStorage } from "../utils/localStorageUtil";
-// import { useNavigate } from "react-router-dom";
-// import routes from "../routes";
-// import { useRouter } from 'next/navigation'
 
 
 export const AxiosInstance = (service) => {
-  let baseURL = `http://127.0.0.1:8000/api/${service}/`;
-  // if (window.location.hostname && window.location.hostname !== 'localhost')
-    // baseURL = `https://sahil11.pythonanywhere.com/api/${service}/`
+  let baseURL = `https://sahil1109.pythonanywhere.com/api/${service}/`;
 
   // const router = useRouter()
   const token = getItemFromLocalStorage("token", "");
-  console.log("request", token)
 
   const axiosInstance = axios.create({
     baseURL: baseURL,
